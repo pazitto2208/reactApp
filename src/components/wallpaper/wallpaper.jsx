00c98bcd554935a89e34d52d69ec3f273ef6b505
Eduardo Pazitto <eduardo.pazitto-neves@electrolux.com>
2023-08-01 09:59:42 +0200

    'home-content'

--- src/components/wallpaper/wallpaper.jsx
+++ src/components/wallpaper/wallpaper.jsx
@@ -5,7 +5,7 @@ function Wallpaper () {
         <div className={styles.wallpaperContainer}>
             <div className={styles.wallpaperContent}>
                 <h1>Benvenuto a <span>costs</span></h1>
-                <p>Puoi iniziare a gestire i tuoi progetti personali con noi</p>
+                <p>Gestisci i tuoi progetti personali con facilità!</p>
             </div>
             <img src="https://images.unsplash.com/photo-1506443432602-ac2fcd6f54e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8&w=1000&q=80" alt="" />
         </div>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,21 @@ function Home() {
         <div className={styles.homeContainer}>
             <Wallpaper />
             <div className={styles.contentContainer}>
-                <h1>ciao</h1>
+                <h1>Organizza, monitora e pianifica con precisione le tue attività!</h1>
+                <p className={styles.contentContainer}>
+                    Costs è un'applicazione innovativa e versatile progettata per aiutarti a tenere traccia di 
+                    tutte le tue attività, pianificare le tue spese e raggiungere i tuoi obiettivi finanziari 
+                    con facilità. Che tu stia pianificando una ristrutturazione domestica, un viaggio indimenticabile 
+                    o un progetto creativo, Costs ti offre le funzionalità essenziali per mantenere tutto sotto controllo 
+                    e raggiungere il successo.
+                </p>
+                <h2>La tua soluzione completa per gestire i progetti personali in modo semplice e intelligente!</h2>
+                <p className={styles.contentContainer}>
+                Creare i tuoi progetti è semplice e veloce: basta assegnare un titolo accattivante e una descrizione 
+                dettagliata per avere una visione chiara e completa di ciò che desideri realizzare. Organizza i tuoi 
+                progetti in categorie personalizzate, come "Casa", "Viaggi", "Hobby" o qualsiasi altra categoria che 
+                rifletta il tuo stile di vita e le tue passioni.
+                </p>
             </div>
         </div>
     )
